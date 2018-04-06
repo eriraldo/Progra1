4853ea48913937b4aa201de7328556d7e2804ba8
dereck <dereck@dereck-pc>
2018-04-05 22:14:59 -0600

    actualizacion

--- Progra 1/src/logica/Counter.java
+++ Progra 1/src/logica/Counter.java
@@ -2,7 +2,6 @@
 package logica;
 
 
-
 public class Counter {
     private String nombre ;
     private String identificacion ;
@@ -65,10 +64,126 @@ public class Counter {
     
     public void addCliente(String name,String id,String mail,String gender,String cellphone,String location,String nacimiento,String rango){
         int largo = listaClientes.length;
-        //System.out.println(largo);
         Cliente nuevo = new Cliente(name,id,mail,gender,cellphone,location,nacimiento,rango);
+        int cont =0;
+        while(cont<largo){
+            if (listaClientes[cont]== null){
+                listaClientes[cont] = nuevo;
+                break;
+                
+            }
+            else
+                cont++;
+                    
+        }
         
-        listaClientes[largo-1] = nuevo;
+                
+    }
+    public void modificarCliente(String cedula,String datoNuevo,int tipoDato){      
+        Cliente temp = new Cliente();
+        int i=0;
+        while(listaClientes[i]!= null){
+            String ced = listaClientes[i].getIdentificador();
+            if (ced == cedula){
+                temp = listaClientes[i];
+                break;
+                
+            }
+            else
+                i++;
+        }
+        switch(tipoDato){                       //Cada tipo de dato tiene un numero de acuerdo al orden del constructor de Cliente
+            case 1:{
+                temp.setNombre(datoNuevo);      //#1  Nombre
+                break;
+                
+            }
+            case 2:{                            //#2 Cedula
+                temp.setIdentificador(datoNuevo);
+                break;
+                
+            }
+            case 3:{
+                temp.setCorreo(datoNuevo);      //#3 Correo
+                break;
+            }
+            case 4:{
+                temp.setSexo(datoNuevo);        //#4 Genero
+                break;
+            }
+            case 5:{
+                temp.setTelefono(datoNuevo);    //#5 telefono
+                break;
+                
+            }
+            case 6:{
+                temp.setDireccion(datoNuevo);   //#6 direccion
+                break;
+            }
+            case 7:{
+                temp.setFechaNacimiento(datoNuevo); //#7 Fecha nacimiento
+                break;
+            }
+            case 8:{
+                temp.setTipoCliente(datoNuevo);     //#8 rango de cliente
+                break;
+            }
+        }
+        listaClientes[i]= temp;
+        System.out.println("Modificado!");
+        
     }
     
+    public void consultarCliente(String cedula){
+        Cliente temp = new Cliente();
+        int i=0;
+        while(listaClientes[i] != null){
+            String ced = listaClientes[i].getIdentificador();
+            if (ced == cedula){
+                temp = listaClientes[i];
+                break;
+                
+            }
+            else
+                i++;
+        }
+        String name = temp.getNombre();
+        String id = temp.getIdentificador();
+        String genero = temp.getSexo();
+        String correo = temp.getCorreo();
+        String telefono = temp.getTelefono();
+        String nacimiento = temp.getFechaNacimiento();
+        String residencia = temp.getDireccion();
+        String tipoCliente = temp.getTipoCliente();
+        
+        System.out.println("   **Datos del Cliente**   ");
+        System.out.println("Nombre: " + name);
+        System.out.println("Identificacion: " + id);
+        System.out.println("Genero: " + genero);
+        System.out.println("Fecha de Nacimiento: " + nacimiento);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Correo: " + correo);
+        System.out.println("Lugar de residencia: " + residencia);
+        System.out.println("Rango de Cliente: " + tipoCliente);
+        
+        
+        
+    }
+    
+    public void eliminarCliente(String cedula){
+        
+        int i=0;
+        while(listaClientes[i] != null){
+            String ced = listaClientes[i].getIdentificador();
+            if (ced == cedula)
+                break;
+            else
+                i++;
+        }
+        listaClientes[i] = null;
+        
+    }
+    
+            
+          
 }
