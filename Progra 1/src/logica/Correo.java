fb8cf0643d3e33c9eeec0da855e7d4dc015a88ce
erick <erick@DESKTOP-7REIBQE.localdomain>
2018-04-06 18:26:45 -0600

    todo con listaCliente

--- Progra 1/src/logica/Correo.java
+++ Progra 1/src/logica/Correo.java
@@ -1,52 +1,85 @@
-/*
+
 package logica;
 
 
-//import java.util.*;
-//import javax.mail.*;
-//import javax.mail.internet.*;
-//import javax.mail.internet.MimeMessage;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
 public class Correo {
 
-//    public static void main(String args[]){
-//        try{
-//            String host ="smtp.gmail.com" ;
-//            String user = "your email address";
-//            String pass = "your password";
-//            String to = "receiever email ";
-//            String from = "sender email";
-//            String subject = "This is confirmation number for your expertprogramming account. Please insert this number to activate your account.";
-//            String messageText = "Your Is Test Email :";
-//            boolean sessionDebug = false;
-//
-//            Properties props = System.getProperties();
-//
-//            props.put("mail.smtp.starttls.enable", "true");
-//            props.put("mail.smtp.host", host);
-//            props.put("mail.smtp.port", "587");
-//            props.put("mail.smtp.auth", "true");
-//            props.put("mail.smtp.starttls.required", "true");
-//
-//            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-//            Session mailSession = Session.getDefaultInstance(props, null);
-//            mailSession.setDebug(sessionDebug);
-//            Message msg = new MimeMessage(mailSession);
-//            msg.setFrom(new InternetAddress(from));
-//            InternetAddress[] address = {new InternetAddress(to)};
-//            msg.setRecipients(Message.RecipientType.TO, address);
-//            msg.setSubject(subject); msg.setSentDate(new Date());
-//            msg.setText(messageText);
-//
-//           Transport transport=mailSession.getTransport("smtp");
-//           transport.connect(host, user, pass);
-//           transport.sendMessage(msg, msg.getAllRecipients());
-//           transport.close();
-//           System.out.println("message send successfully");
-//        }catch(Exception ex)
-//        {
-//            System.out.println(ex);
-//        }
-//
-//    }
+    public static void main(String args[])
+    {
+        try
+        {
+            String host ="smtp.gmail.com" ;
+            String user = "progradepoo1@gmail.com";
+            String pass = "progradePOO";
+            String to = "ericksalazar_g@hotmail.com ";
+            String to1 = "dereckescalante@hotmail.com";
+            //String to2 = "jopablox99@gmail.com";
+            String from = "progradepoo1@gmail.com";
+            String subject = "una mas";
+            String messageText = "anemonas 69";
+            boolean sessionDebug = false;
+
+            Properties props = System.getProperties();//para definir direntes tipos de propiedades
+
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.required", "true");
+            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+            
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            //se necesita seguridad, pero java ya nos da seguridad
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(sessionDebug);
+            Message msg = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(from));//correo de la persona que lo manda
+            InternetAddress[] address = {new InternetAddress(to)};//correo de la persona que lo recibe
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject(subject); msg.setSentDate(new Date());//fecha del evio del correo
+            msg.setText(messageText);//mensaje actual
+
+           Transport transport=mailSession.getTransport("smtp");//servidor por el que se va a mandar el correo
+           transport.connect(host, user, pass);//se necesita autenticar el usuario y la contrasena
+           transport.sendMessage(msg, msg.getAllRecipients());
+           transport.close();
+           System.out.println("message send successfully");
+           
+           //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            //se necesita seguridad, pero java ya nos da seguridad
+            Session mailSession2 = Session.getDefaultInstance(props, null);
+            mailSession2.setDebug(sessionDebug);
+            Message msg2 = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(from));//correo de la persona que lo manda
+            InternetAddress[] address2 = {new InternetAddress(to1)};//correo de la persona que lo recibe
+            msg.setRecipients(Message.RecipientType.TO, address2);
+            msg.setSubject(subject); msg.setSentDate(new Date());//fecha del evio del correo
+            msg.setText(messageText);//mensaje actual
+
+           Transport transport2=mailSession.getTransport("smtp");//servidor por el que se va a mandar el correo
+           transport.connect(host, user, pass);//se necesita autenticar el usuario y la contrasena
+           transport.sendMessage(msg, msg.getAllRecipients());
+           transport.close();
+           System.out.println("message send successfully");
+           
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex);//si algo pasa con el envio del correo tira un mensaje
+        }
+
+    }
 }
-*/
+
+
+
+
